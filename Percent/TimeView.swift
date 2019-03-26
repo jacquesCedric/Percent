f44a04583b7b8732ddcdc4763e4e6ea927d72932
Jacob <12831497+jacquesCedric@users.noreply.github.com>
2019-03-26 15:05:32 +1100

    Swift 5 update. Minor Percent view adjustments

--- Percent/TimeView.swift
+++ Percent/TimeView.swift
@@ -19,16 +19,17 @@ class TimeView: NSView {
     
     //or customized constructor/ init
     init(period: PercentType) {
-        let size = NSRect(x: 0, y: 0, width: 200, height: 24)
+        let size = NSRect(x: 0, y: 0, width: 115, height: 24)
         
         super.init(frame: size)
         
-        let progress: JCGGProgressBar = JCGGProgressBar.init(frame: CGRect(x: 0, y: 4, width: 80, height: 16))
+        let progress: JCGGProgressBar = JCGGProgressBar.init(frame: CGRect(x: 0, y: 8, width: 40, height: 14))
         progress.progressValue = CGFloat(getPercent(period: period))
+        progress.barThickness = 14
         
         let label: NSTextField = NSTextField.init(labelWithString: getPercentLabel(period: period))
-        label.frame = NSRect(x: 85, y: 0, width: 115, height: 24)
-        label.font = NSFont.systemFont(ofSize: 14)
+        label.frame = NSRect(x: 45, y: 0, width: 70, height: 24)
+        label.font = NSFont.menuBarFont(ofSize: 0)
         
         self.addSubview(progress)
         self.addSubview(label)
